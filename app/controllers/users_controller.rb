6804fd6ab3a55d6f99baffd2c5902149862bebe5
txwildcat <tyler.askren@kellogg.northwestern.edu>
2022-03-13 21:46:52 +0000

    authentication might be done?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params["id"])
+  end
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(params["user"])
+    @user.password = BCrypt::Password.create(params["user"]["password"])
     @user.save
-    redirect_to "/"
+    redirect_to "/users/#{@user.id}"
   end
 end
