01af10703941ac85b80dfc6c75517fc908da1ddc
txwildcat <tyler.askren@kellogg.northwestern.edu>
2022-03-13 22:48:21 +0000

    more progress 2.17

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   # end
 
   def current_user
-    if session ["user_id"]
+    # if session ["user_id"]
       @current_user = User.find(session[:user_id]) if session[:user_id]
-    end
+    # end
   end
 end
