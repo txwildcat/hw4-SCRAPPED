beab332d64a8a9c96fa8c814de090e38db5ef8c4
txwildcat <tyler.askren@kellogg.northwestern.edu>
2022-03-13 23:36:09 +0000

    this is seeming to work?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if @user
       if BCrypt::Password.new(@user.password) == params["password"]
         session[:user_id] = @user.id
-        flash[:notice] = "Welcome, #{@user.first_name}."
+        flash[:notice] = "Welcome, #{@user.username}."
         redirect_to "/places"
       else
         flash[:notice] = "Nope"
