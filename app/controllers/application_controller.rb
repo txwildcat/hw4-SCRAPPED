7375bc34bb762a3c5bef56fe260276d897eccff0
txwildcat <tyler.askren@kellogg.northwestern.edu>
2022-03-14 00:59:47 +0000

    commenting back in application controller pre give

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :current_user
 
-  # def current_user
-  #   puts "------------------ code before every request ------------------"
-  # end
-
   def current_user
-    # if session ["user_id"]
+    puts "------------------ code before every request ------------------"
+    if session ["user_id"]
       @current_user = User.find(session[:user_id]) if session[:user_id]
-    # end
+    end
   end
 end
